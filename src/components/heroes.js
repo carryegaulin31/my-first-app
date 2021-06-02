584a47544549100266a33a08563f3509e9f68d6b
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-06-01 20:26:44 -0400

    initial commit

---
+++ src/components/heroes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heroes = () => {
+  return (
+    <div className='App'>
+      <h2>These are our heroes</h2>
+      <div>Meet Our Heroes</div>
+    </div>
+  )
+}
+
+export default Heroes;
